3b8cbee3f654fbe62fb0dd69066a110c948d9d88
Nick Zinovenko <8070617+exscriber@users.noreply.github.com>
2025-02-27 12:18:09 +0300

    bugfix

--- video-Hap/to-Hap-fix-Res.cmd
+++ video-Hap/to-Hap-fix-Res.cmd
@@ -5,7 +5,7 @@ set params=-c:v hap -format hap -c:a pcm_s16le -ar 44100
 :: set filter=scale=force_original_aspect_ratio=increase:force_divisible_by=4
 :: set filter=scale=force_original_aspect_ratio=decrease:force_divisible_by=4
 :: set filter=pad=ceil(iw/4)*4:ceil(ih/4)*4:(ow-iw)/2:(oh-ih)/2
-set filter=crop=floor(iw/4)*4:floor(ih/4)*4:(ow-iw)/2:(oh-ih)/2
+set filter=crop=floor(iw/4)*4:floor(ih/4)*4
 
 mkdir !enc
 
